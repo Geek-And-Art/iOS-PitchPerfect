e7e7d32086404f6f69c4f3ff5242170678d351db
Tao Xie <xietao26@gmail.com>
2016-11-30 20:53:21 +0800

    Add the feature of banning.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
-
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,19 +23,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordingButton.isEnabled = false
+    }
 
     @IBAction func recordAudio(_ sender: Any) {
-        print("record button pressed!")
+        print("record button pressed")
         recordingLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordingButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop recording button pressed!")
+        print("stop recording button pressed")
+        recordingButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        print("viewWillAppear called")
-    }
-
 }
 
